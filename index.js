6bdd6bc03ef9583cbeffddef41aaf5796346282e
zakizaki-ri9 <kic.yuuki@gmail.com>
2022-01-18 22:15:41 +0900

    udpate md-to-pdf

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import mtp from "md-to-pdf";
+import { mdToPdf } from "md-to-pdf";
 
-mtp("./README.md", { dest: "README.pdf" }).then(() => {
-  console.log("mtp Done");
+mdToPdf({ path: "./README.md" }, { dest: "README.pdf" }).then(() => {
+  console.log("Create pdf done");
 });
